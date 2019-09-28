36ab07fb9eb97b96f1ee04ac54c1ddfe2129c848
Gaurav Gambhir <gaurav.gambhir.er@gmail.com>
2019-09-28 10:57:26 +0530

    added reame.txt

---
+++ index.js
@@ -0,0 +1,215 @@
+let {ExtendedLogger} = require("@logger");
+let errorCodes       = require("@error-messages");
+let logger           = new ExtendedLogger("api-body-validator");
+const validator      = require('validator');
+
+/**
+ * @param {Object} validationCriteria => Validation Criteria
+ *          @sample {
+                "body" : [{
+                    "email" : {
+                    "$req" : true,
+                    "$pattern" : "[a-z0-9]+@[a-z0-9]+\.com"
+                    }
+                }],
+                "params" : [{
+                    "exlOrPpt" : {
+                    "$in" : ["xlsx", "pptx"]
+                    }
+                }]
+                }
+*/
+module.exports.validate = function(validationCriteria) {
+    return function (req, res, next) {
+        for (let validationKey in validationCriteria) {
+            if (!(validationCriteria[validationKey] instanceof Array)) {
+                throw new Error(`Validation criteria for request ${validationKey} must be an array` )
+            } else {
+                let {isValid, errorMessage} = validateBody(req, validationCriteria[validationKey], validationKey, validationKey);
+                if (!isValid) {
+                    generateErrorResponse(res, errorMessage);return;
+                }
+            }
+
+        }
+        next();
+    }
+}
+/**
+ * Function that returns value of nested key
+ * @param {Object} obj Object to get nested field from
+ * @param {String} key Dot separated nested keys
+ * @author Swapnil Kumar(techgenies.com)
+ */
+const getNestedField = (obj,key) => {
+    logger.debug(key);
+    let arr = key.split('.');
+    for (let k of arr) {
+        obj = obj[k];
+        console.log(obj);
+        logger.debug("key and object", k, obj);
+        if(typeof obj === "undefined") {
+            return obj;
+        }
+    }
+    return obj;
+}
+/**
+ * Function to perform type checking
+ * @param {*} val Val to check type of
+ * @param {*} type Type to check against
+ * @returns {Boolean} Returns a boolean representing the type checking status
+ * @author Swapnil Kumar(techgenies.com)
+ */
+const typeCheck = (val, type) => {
+
+    if(typeof(val) == "undefined"){
+        return false;
+    }
+
+    switch(type)
+    {
+        case "number":
+            return validator.isNumeric(val.toString());
+        case "boolean":
+            return validator.isBoolean(val.toString());
+        case "string":
+            return typeof(val) == "string";
+        case "object":
+            return typeof(val) == "object";
+        case "email":
+            return validator.isEmail(val.toString());
+        case "array":
+            return Array.isArray(val);
+    }
+    return true;
+}
+
+/**
+ *
+ * @param {*} body => Object to validate
+ * @param {*} whereInRequest  => Where the value is coming from body, query or params.
+ * @author Swapnil Kumar(techgenies.com)
+ */
+const validateBody = function(req, body, whereInRequest, validationPath) {
+    console.log(req);
+    logger.debug(body, whereInRequest);
+    let isValid = true;
+    for (let i=0;i<body.length;i++) {
+       let validationCriteria = body[i];
+       for (let validationFeild in validationCriteria) {
+           let validation = validationCriteria[validationFeild];
+           logger.debug("validationField: ",validationFeild);
+           let actualValue = getNestedField(req[whereInRequest], validationFeild);
+           let isUndefined = (typeof(actualValue) === "undefined");
+           logger.debug("isUndefined", isUndefined, actualValue);
+           if (validation.$req === true) {
+                if (!isUndefined) {
+                    isValid = true;
+                } else {
+                    isValid = false;
+                    return {
+                        isValid,
+                        errorMessage : `Data in  ${validationPath}.${validationFeild} is required`
+                    }
+                }
+           }
+           //for all fields inside the object which doesn't start with a $ is a nested criteria
+           let onlyHasDollarReq = true;
+           let hasNestedCondtion = false;
+           let nonDollarRequestField = "";
+           for (let key in validation) {
+                if(isUndefined){
+                    continue;
+                }
+
+                if (!key.startsWith("$")) {
+                    hasNestedCondtion = true;
+                    logger.debug("key", key, validation);
+                    let criteria = {};
+                    criteria[key] = validation[key];
+                    let tempReq = {};
+                    tempReq[validationFeild] = req[whereInRequest][validationFeild];
+                    let temp = validateBody(tempReq, [criteria], validationFeild, `${validationPath}.${validationFeild}`);
+                    isValid = temp.isValid;
+                    if (!isValid) {
+                        return temp;
+                    }
+                } else {
+                    if (key != "$req") {
+                        nonDollarRequestField = key;
+                        onlyHasDollarReq = false;
+                    }
+
+                }
+           }
+           logger.debug(hasNestedCondtion, onlyHasDollarReq);
+           if ((hasNestedCondtion && onlyHasDollarReq) || !hasNestedCondtion) {
+            if (!isUndefined && validation.$type) {
+                if (typeCheck(actualValue, validation.$type)) {
+                    isValid = true;
+                } else {
+                    isValid = false;
+                    return {
+                         isValid,
+                         errorMessage : `Data type mismatch for  ${validationPath}.${validationFeild} should be ${validation.$type}`
+                     }
+                }
+            }
+            if (!isUndefined && validation.$eq) {
+                 if (validation.$eq === actualValue) {
+                     isValid = true;
+                 } else {
+                     isValid = false;
+                     return {
+                         isValid,
+                         errorMessage : `Data in  ${validationPath}.${validationFeild} cannot be ${actualValue}`
+                     }
+                 }
+             }
+             if (!isUndefined && validation.$in) {
+                 if (validation.$in.indexOf(actualValue) > -1) {
+                     isValid = true;
+                 } else {
+                     isValid = false;
+                     return {
+                         isValid,
+                         errorMessage : `Data in  ${validationPath}.${validationFeild} cannot be ${actualValue}`
+                     }
+                 }
+             }
+             if (!isUndefined && validation.$pattern) {
+                 let regExp = new RegExp(validation.$pattern);
+                 if (actualValue.match(regExp) && actualValue.match(regExp).length > 0) {
+                     isValid = true;
+                 } else {
+                     isValid = false;
+                     return {
+                         isValid,
+                         errorMessage : `Data in  ${validationPath}.${validationFeild} does not match ${validation.$pattern}`
+                     }
+                 }
+             }
+
+        } else {
+            if (!onlyHasDollarReq) {
+                throw new Error(`Nested Criteria only validates $req for the parent found ${nonDollarRequestField}`);
+            }
+
+        }
+       }
+    }
+    logger.debug("passed api validation!");
+    return {isValid};
+}
+/**
+ *
+ * @param {*} errorMessage => errorMessage to send
+ */
+const generateErrorResponse = function(res, errorMessage) {
+
+    res.status(400).send({
+        "success" : false,
+        "error" : {errorCode : "ERR002",   errorMessage }
+    })
+}
---
+++ readme.txt
@@ -0,0 +1,90 @@
+# api-schema-validator
+
+api-schema-validator is an express middleware designed to validate request body, params, cookies, tokens and query. If you care using special middleware which adds properties to request, this middleware can be used to validate that.
+
+## Installation
+
+npm i api-schema-validator
+
+## Usage
+
+const {validate} = require("api-schema-validator");
+
+req.post("/path", validate({
+               "body" : [{
+                   "email" : {
+                     "$req" : true,
+                     "$pattern" : "[a-z0-9]+@[a-z0-9]+\.com"
+                   }
+               }],
+               "params" : [{
+                   "fileType" : {
+                    "$in" : ["xlsx", "pptx"]
+                   }
+               }]
+               }),
+               function(req, res) {
+                  res.status(200).send({
+                    "status" : "success",
+                    "message" : "Request schema validated successfully"
+                  })
+               })
+## Abbreviation used for this documentaion
+
+body/param/query/cookie : bpqc
+
+## Available options
+
+1. "$eq"     : Checks the value of any field in bpqc equals a value
+2. "$in"     : Allows value in a bpqc field from a given option
+3. "$req"    : Defines if parameter is required or not
+4. "$type"   : accepts number|boolean|array|object|email
+5. "$patter" : Validates bpqc field to be in a specific regular expression field
+6. For nested body paramter validations refer to examples below
+
+6.a using dot notation
+
+  Below example ensures req.body.level1 and request.body.level1.level2 are present in the request body
+  req.post("/path", validate({
+               "body" : [{
+                      "level1.level2" : {
+                        "$req" : true
+                      }
+                   }]
+               }),
+               function(req, res) {
+                  res.status(200).send({
+                    "status" : "success",
+                    "message" : "Request schema validated successfully"
+                  })
+               })
+
+6.b using nested structure
+  Below example ensures if req.body.level1 is present, request.body.level1.level2 must be present in the request body
+  req.post("/path", validate({
+               "body" : [{
+                      "level1" : {
+                        "$req" : false,
+                        "level2" : {
+                          "$req" : true
+                        }
+                      }
+                   }]
+               }),
+               function(req, res) {
+                  res.status(200).send({
+                    "status" : "success",
+                    "message" : "Request schema validated successfully"
+                  })
+               })
+
+  Note : 1. Validation criteria that does not start with "$" is treated as request bpqc field.
+         2. For nested criteria, the parent can only validate with "$req"
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
